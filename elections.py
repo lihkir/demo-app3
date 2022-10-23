1d8463d68c8073b1613ffbdb5735d5a2431d6c0f
lihkir <32485487+lihkir@users.noreply.github.com>
2022-10-23 09:23:55 -0500

    Update elections.py

--- elections.py
+++ elections.py
@@ -6,9 +6,7 @@ import dash_bootstrap_components as dbc
 import pandas as pd
 import plotly.express as px
 
-df = pd.read_csv('politics.csv')
-#you can also read your data like this:
-# df = pd.read_csv('/home/charmingdata1/demo-app3/politics.csv')
+df = pd.read_csv('https://raw.githubusercontent.com/lihkir/Data/main/politics.csv')
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 # server = app.server
